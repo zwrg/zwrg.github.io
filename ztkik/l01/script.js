5275d18d5ea39c7c98becb1c34743fc5d72dea58
zwrg <riper384@gmail.com>
2020-10-17 14:30:04 +0200

    added l02

--- ztkik/l01/script.js
+++ ztkik/l01/script.js
@@ -1,194 +1,196 @@
 const toDec = (numberString, system) => {
-	numberString = numberString.toUpperCase();
-	let value = 0;
-	let base = 1;
-	const length = numberString.length;
-	for (let i = length - 1; i >= 0; i--) {
-		parsedNumericValue = numberString[i];
-		if (system === 16) {
-			parsedNumericValue = hexToDecConversion(numberString[i]);
-		}
+    numberString = numberString.toUpperCase();
+    let value = 0;
+    let base = 1;
+    const length = numberString.length;
+    let parsedNumericValue;
+    for (let i = length - 1; i >= 0; i--) {
+        parsedNumericValue = numberString[i];
+        if (system === 16) {
+            parsedNumericValue = hexToDecConversion(numberString[i]);
+        }
 
-		if (validInput(numberString[i], system)) {
-			value += parsedNumericValue * base;
-			base *= system;
-		} else {
-			console.warn(
-				`not valid character ${numberString[i]}, for system ${system}`,
-			);
-			alert(
-				`invalid input for number conversion // ${numberString}(${system})`,
-			);
-			return;
-		}
-	}
-	return value;
+        if (validInput(numberString[i], system)) {
+            value += parsedNumericValue * base;
+            base *= system;
+        } else {
+            console.warn(
+                `not valid character ${numberString[i]}, for system ${system}`,
+            );
+            alert(
+                `invalid input for number conversion // ${numberString}(${system})`,
+            );
+            return;
+        }
+    }
+    return value;
+    f
 };
 
 const fromDec = (number, toSystem) => {
-	let tmpNumber = number;
-	const array = [];
-	for (let i = 0; tmpNumber > 0; i++) {
-		let modulo = tmpNumber % toSystem;
-		if (toSystem == 16) {
-			modulo = decToHexConversion(modulo);
-		}
-		array.push(modulo);
-		tmpNumber = Math.floor(tmpNumber / toSystem);
-	}
-	return array.reverse().join('');
+    let tmpNumber = number;
+    const array = [];
+    for (let i = 0; tmpNumber > 0; i++) {
+        let modulo = tmpNumber % toSystem;
+        if (toSystem == 16) {
+            modulo = decToHexConversion(modulo);
+        }
+        array.push(modulo);
+        tmpNumber = Math.floor(tmpNumber / toSystem);
+    }
+    return array.reverse().join('');
 };
 const validInput = (input, system) => {
-	return systems[system].numbers.some((n) => n == input);
+    return systems[system].numbers.some((n) => n == input);
 };
 const hexToDecConversion = (hex) => {
-	switch (hex) {
-		case 'A':
-			return 10;
-		case 'B':
-			return 11;
-		case 'C':
-			return 12;
-		case 'D':
-			return 13;
-		case 'E':
-			return 14;
-		case 'F':
-			return 15;
-		default:
-			return hex;
-	}
+    switch (hex) {
+        case 'A':
+            return 10;
+        case 'B':
+            return 11;
+        case 'C':
+            return 12;
+        case 'D':
+            return 13;
+        case 'E':
+            return 14;
+        case 'F':
+            return 15;
+        default:
+            return hex;
+    }
 };
 
 const decToHexConversion = (dec) => {
-	switch (dec) {
-		case 10:
-			return 'A';
-		case 11:
-			return 'B';
-		case 12:
-			return 'C';
-		case 13:
-			return 'D';
-		case 14:
-			return 'E';
-		case 15:
-			return 'F';
-		default:
-			return dec;
-	}
+    switch (dec) {
+        case 10:
+            return 'A';
+        case 11:
+            return 'B';
+        case 12:
+            return 'C';
+        case 13:
+            return 'D';
+        case 14:
+            return 'E';
+        case 15:
+            return 'F';
+        default:
+            return dec;
+    }
 };
 
 const systems = {
-	2: {
-		base: 2,
-		fromDec: (number) => fromDec(number, 2),
-		toDec: (number) => toDec(number, 2),
-		numbers: [0, 1],
-	},
-	3: {
-		base: 3,
-		fromDec: (number) => fromDec(number, 3),
-		toDec: (number) => toDec(number, 3),
-		numbers: [0, 1, 2],
-	},
-	8: {
-		base: 8,
-		fromDec: (number) => fromDec(number, 8),
-		toDec: (number) => toDec(number, 8),
-		numbers: [0, 1, 2, 3, 4, 5, 6, 7],
-	},
-	10: {
-		base: 10,
-		fromDec: (number) => fromDec(number, 10),
-		toDec: (number) => toDec(number, 10),
-		numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-	},
-	16: {
-		base: 16,
-		fromDec: (number) => fromDec(number, 16),
-		toDec: (number) => toDec(number, 16),
-		numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'],
-	},
+    2: {
+        base: 2,
+        fromDec: (number) => fromDec(number, 2),
+        toDec: (number) => toDec(number, 2),
+        numbers: [0, 1],
+    },
+    3: {
+        base: 3,
+        fromDec: (number) => fromDec(number, 3),
+        toDec: (number) => toDec(number, 3),
+        numbers: [0, 1, 2],
+    },
+    8: {
+        base: 8,
+        fromDec: (number) => fromDec(number, 8),
+        toDec: (number) => toDec(number, 8),
+        numbers: [0, 1, 2, 3, 4, 5, 6, 7],
+    },
+    10: {
+        base: 10,
+        fromDec: (number) => fromDec(number, 10),
+        toDec: (number) => toDec(number, 10),
+        numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    },
+    16: {
+        base: 16,
+        fromDec: (number) => fromDec(number, 16),
+        toDec: (number) => toDec(number, 16),
+        numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'],
+    },
 };
 
 const ex01 = () => {
-	const number = document.querySelector('#ex01number').value;
-	const system = document.querySelector('#ex01system').value;
-	let results = '';
-	const resultDiv = document.querySelector('#ex01results');
+    const number = document.querySelector('#ex01number').value;
+    const system = document.querySelector('#ex01system').value;
+    let results = '';
+    const resultDiv = document.querySelector('#ex01results');
 
-	const decimalNumber = systems[system].toDec(number);
+    const decimalNumber = systems[system].toDec(number);
 
-	for (sys in systems) {
-		results += `System(${systems[sys].base}) = ${systems[sys].fromDec(
-			decimalNumber,
-			systems[sys].base,
-		)} \n`;
-	}
-	resultDiv.textContent = results;
+    for (sys in systems) {
+        results += `System(${systems[sys].base}) = ${systems[sys].fromDec(
+            decimalNumber,
+            systems[sys].base,
+        )} \n`;
+    }
+    resultDiv.textContent = results;
 };
 
 const clearex01 = () => {
-	document.querySelector('#ex01number').value = '';
-	document.querySelector('#ex01system').value = 2;
-	document.querySelector('#ex01results').textContent = '';
+    document.querySelector('#ex01number').value = '';
+    document.querySelector('#ex01system').value = 2;
+    document.querySelector('#ex01results').textContent = '';
 };
 
 const gcdeuclid_extended = (a, b) => {
-	let iterations = 0;
-	/*quotients definition*/
-	let s = 0,
-		old_s = 1;
-	let t = 1,
-		old_t = 0;
-	let r = b,
-		old_r = a;
-	while (r !== 0) {
-		iterations++;
-		let quotient = Math.floor(old_r / r);
+    let iterations = 0;
+    /*quotients definition*/
+    let s = 0,
+        old_s = 1;
+    let t = 1,
+        old_t = 0;
+    let r = b,
+        old_r = a;
+    while (r !== 0) {
+        iterations++;
+        let quotient = Math.floor(old_r / r);
 
-		[old_r, r] = [r, old_r - quotient * r];
-		[old_s, s] = [s, old_s - quotient * s];
-		[old_t, t] = [t, old_t - quotient * t];
-	}
-	/*
+        [old_r, r] = [r, old_r - quotient * r];
+        [old_s, s] = [s, old_s - quotient * s];
+        [old_t, t] = [t, old_t - quotient * t];
+    }
+    /*
      old_s, old_t - Bezout coefficients
-	 old_r - greatest common divisor
-	 t, s - quotients by gcd
+     old_r - greatest common divisor
+     t, s - quotients by gcd
     */
-	return [old_t, old_s, old_r, iterations];
+    return [old_t, old_s, old_r, iterations];
 };
 
 const ex02 = () => {
-	const a = Number.parseInt(document.querySelector('#ex02euclid_a').value);
-	const b = Number.parseInt(document.querySelector('#ex02euclid_b').value);
-	let results = '';
-	if (!a || !b) return;
-	const resultDiv = document.querySelector('#ex02results');
+    const a = Number.parseInt(document.querySelector('#ex02euclid_a').value);
+    const b = Number.parseInt(document.querySelector('#ex02euclid_b').value);
+    let results = '';
+    if (!a || !b) return;
+    const resultDiv = document.querySelector('#ex02results');
 
-	let [u, v, gcd, iterations] = gcdeuclid_extended(a, b);
+    let [u, v, gcd, iterations] = gcdeuclid_extended(a, b);
 
-	console.log('number of iterations =>', iterations);
+    console.log('number of iterations =>', iterations);
 
-	results += `nwd = ${gcd} \n\n`;
-	results += `a = ${a} \n`;
-	results += `t or u = ${u} \n\n`;
-	results += `b = ${b} \n`;
-	results += `s or v = ${v} \n\n`;
-	results += `ux + vy = nwd(x,y) \n`;
-	results += `${a} * (${v}) + ${b} * (${u}) = ${gcd} \n`;
-	const inverseElement =
-		gcd === 1
-			? `1/${b} = ${b}^-1 is congurent to ${u}(mod ${a}) \n`
-			: 'no inverse element gcd != 1';
+    results += `nwd = ${gcd} \n\n`;
+    results += `a = ${a} \n`;
+    results += `t or u = ${u} \n\n`;
+    results += `b = ${b} \n`;
+    results += `s or v = ${v} \n\n`;
+    results += `ux + vy = nwd(x,y) \n`;
+    results += `${a} * (${v}) + ${b} * (${u}) = ${gcd} \n`;
+    const inverseElement =
+        gcd === 1
+            ? `1/${b} = ${b}^-1 is congurent to ${u}(mod ${a}) \n`
+            : 'no inverse element gcd != 1';
 
-	results += inverseElement;
-	resultDiv.textContent = results;
+    results += inverseElement;
+    resultDiv.textContent = results;
 };
 
 const clearex02 = () => {
-	document.querySelector('#ex02euclid_a').value = '';
-	document.querySelector('#ex02euclid_b').value = '';
-	document.querySelector('#ex02results').textContent = '';
+    document.querySelector('#ex02euclid_a').value = '';
+    document.querySelector('#ex02euclid_b').value = '';
+    document.querySelector('#ex02results').textContent = '';
 };
---
+++ ztkik/l02/script.js
@@ -0,0 +1,460 @@
+const arrays = [[
+    {char: 'a', key: 'e'},
+    {char: 'ą', key: 'q'},
+    {char: 'b', key: 'ń'},
+    {char: 'c', key: 'y'},
+    {char: 'ć', key: 'j'},
+    {char: 'd', key: 'r'},
+    {char: 'e', key: 'ł'},
+    {char: 'ę', key: 'ó'},
+    {char: 'f', key: 'k'},
+    {char: 'g', key: 'u'},
+    {char: 'h', key: 'w'},
+    {char: 'i', key: 'd'},
+    {char: 'j', key: 'z'},
+    {char: 'k', key: 'f'},
+    {char: 'l', key: 'p'},
+    {char: 'ł', key: 'ą'},
+    {char: 'm', key: 'ż'},
+    {char: 'n', key: 'ś'},
+    {char: 'ń', key: 'ę'},
+    {char: 'o', key: 't'},
+    {char: 'ó', key: 'l'},
+    {char: 'p', key: 'ź'},
+    {char: 'q', key: 'i'},
+    {char: 'r', key: 'n'},
+    {char: 's', key: 'a'},
+    {char: 'ś', key: 's'},
+    {char: 't', key: 'c'},
+    {char: 'u', key: 'ó'},
+    {char: 'w', key: 'b'},
+    {char: 'y', key: 'h'},
+    {char: 'z', key: 'm'},
+    {char: 'ż', key: 'ć'},
+    {char: 'ź', key: 'g'},
+    {char: ' ', key: ' '}
+], [
+    {char: 'a', key: 11},
+    {char: 'ą', key: 20},
+    {char: 'b', key: 27},
+    {char: 'c', key: 9},
+    {char: 'ć', key: 32},
+    {char: 'd', key: 23},
+    {char: 'e', key: 13},
+    {char: 'ę', key: 28},
+    {char: 'f', key: 16},
+    {char: 'g', key: 3},
+    {char: 'h', key: 31},
+    {char: 'i', key: 19},
+    {char: 'j', key: 8},
+    {char: 'k', key: 24},
+    {char: 'l', key: 30},
+    {char: 'ł', key: 2},
+    {char: 'm', key: 26},
+    {char: 'n', key: 6},
+    {char: 'ń', key: 4},
+    {char: 'o', key: 25},
+    {char: 'ó', key: 15},
+    {char: 'p', key: 1},
+    {char: 'q', key: 14},
+    {char: 'r', key: 12},
+    {char: 's', key: 18},
+    {char: 'ś', key: 5},
+    {char: 't', key: 21},
+    {char: 'u', key: 7},
+    {char: 'w', key: 17},
+    {char: 'y', key: 0},
+    {char: 'z', key: 22},
+    {char: 'ż', key: 29},
+    {char: 'ź', key: 10},
+    {char: ' ', key: ' '}
+], [
+    {char: 'a', key: 0},
+    {char: 'ą', key: 1},
+    {char: 'b', key: 2},
+    {char: 'c', key: 3},
+    {char: 'ć', key: 4},
+    {char: 'd', key: 5},
+    {char: 'e', key: 6},
+    {char: 'ę', key: 7},
+    {char: 'f', key: 8},
+    {char: 'g', key: 9},
+    {char: 'h', key: 10},
+    {char: 'i', key: 11},
+    {char: 'j', key: 12},
+    {char: 'k', key: 13},
+    {char: 'l', key: 14},
+    {char: 'ł', key: 15},
+    {char: 'm', key: 16},
+    {char: 'n', key: 17},
+    {char: 'ń', key: 18},
+    {char: 'o', key: 19},
+    {char: 'ó', key: 20},
+    {char: 'p', key: 21},
+    {char: 'q', key: 22},
+    {char: 'r', key: 23},
+    {char: 's', key: 24},
+    {char: 'ś', key: 25},
+    {char: 't', key: 26},
+    {char: 'u', key: 27},
+    {char: 'w', key: 28},
+    {char: 'y', key: 29},
+    {char: 'z', key: 30},
+    {char: 'ż', key: 31},
+    {char: 'ź', key: 32},
+    {char: ' ', key: ' '},
+]
+]
+
+const EX03ARRAY = 1;
+
+/*
+LICZBA BITÓW
+PREDYKCJA BIEŻĄCA
+
+YWTMŁŚDŁBRŁAMYM
+ĆEMREFTŚŚE
+ */
+
+/*
+SPRZEDAJ SŁONINĘ
+JUTRO TA GODZINA
+
+LUMBLWNHLA
+NHWĄJFIĄŻĘ
+ */
+
+/*
+ZAKOPANE
+MRĄGOWOJAD
+
+COUFŚI
+GRYŚYDY
+ */
+
+const letterIsInArrayEx01 = (letter, encode = true) => {
+    return encode ? arrays[0].some(value => value.char === letter) : arrays[0].some(value => `${value.key}` === letter)
+}
+
+const letterIsInArrayEx02 = (letter) => {
+    return arrays[1].some(value => value.char === letter);
+}
+
+const operation = (encode, exercise) => {
+    switch (exercise) {
+        case 1:
+            ex01(encode, exercise);
+            break;
+        case 2:
+            ex02(encode, exercise);
+            break;
+        case 3:
+            ex03(encode, exercise);
+            break;
+    }
+
+}
+
+const ex01 = (encode, exercise = 1) => {
+    const text = document.querySelector(`#ex0${exercise}text`).value.toLowerCase();
+    const resultDiv = document.querySelector(`#ex0${exercise}results`);
+
+    const textArray = text.split('');
+    const resultArray = [];
+    if (textArray.every(value => letterIsInArrayEx01(value, encode))) {
+        textArray.forEach(letter => {
+            const swapLetter = arrays[exercise - 1].find(value => (encode ? value.char : `${value.key}`) === letter)
+            resultArray.push((encode ? swapLetter.key : swapLetter.char));
+        })
+    } else {
+        alert('text not in array');
+        console.warn('text not in array');
+    }
+
+    resultDiv.textContent = resultArray.join('').toUpperCase();
+}
+
+const gcd_extended_invert_a = (a, b) => {
+    let iterations = 0;
+    /*quotients definition*/
+    let s = 0,
+        old_s = 1;
+    let t = 1,
+        old_t = 0;
+    let r = b,
+        old_r = a;
+    while (r !== 0) {
+        iterations++;
+        let quotient = Math.floor(old_r / r);
+
+        [old_r, r] = [r, old_r - quotient * r];
+        [old_s, s] = [s, old_s - quotient * s];
+        [old_t, t] = [t, old_t - quotient * t];
+    }
+    /*
+     old_s, old_t - Bezout coefficients
+     old_r - greatest common divisor
+     t, s - quotients by gcd
+    */
+    return old_t;
+};
+
+function calculate_modulo(exercise, letterValue) {
+    const a = Number.parseInt(document.querySelector(`#ex0${exercise}a`).value);
+    const b = Number.parseInt(document.querySelector(`#ex0${exercise}b`).value);
+    const l = Number.parseInt(document.querySelector(`#ex0${exercise}l`).value);
+    return (a * letterValue + b) % l;
+}
+
+function calculate_invert_modulo(exercise, letterValue) {
+    const a = Number.parseInt(document.querySelector(`#ex0${exercise}a`).value);
+    const b = Number.parseInt(document.querySelector(`#ex0${exercise}b`).value);
+    const l = Number.parseInt(document.querySelector(`#ex0${exercise}l`).value);
+    let invert = gcd_extended_invert_a(l, a);
+    while (invert <= 0) {
+        invert += l;
+    }
+    return (invert * (letterValue - b)) % l;
+}
+
+
+function encodeLetter(exercise, letter) {
+    const letterObject = arrays[exercise - 1].find(value => value.char === letter);
+    const letterValue = letterObject.key;
+    const encodedValue = calculate_modulo(exercise, letterValue);
+    return arrays[exercise - 1].find(value => value.key === encodedValue)?.char;
+}
+
+function decodeLetter(exercise, letter) {
+    const letterObject = arrays[exercise - 1].find(value => value.char === letter);
+    const letterValue = letterObject.key;
+    const decodedValue = calculate_invert_modulo(exercise, letterValue);
+    return arrays[exercise - 1].find(value => value.key === decodedValue)?.char;
+
+}
+
+const ex02 = (encode, exercise = 2) => {
+    const text = document.querySelector(`#ex0${exercise}text`).value.toLowerCase();
+    const resultDiv = document.querySelector(`#ex0${exercise}results`);
+
+    const textArray = text.split('');
+    const resultArray = [];
+    if (textArray.every(value => letterIsInArrayEx02(value))) {
+        textArray.forEach(letter => {
+            const codedLetter = encode ? encodeLetter(exercise, letter) : decodeLetter(exercise, letter);
+            resultArray.push(codedLetter);
+        })
+    } else {
+        alert('text not in array');
+        console.warn('text not in array');
+    }
+
+    resultDiv.textContent = resultArray.join('').toUpperCase();
+}
+
+function calculate_encoded_vector(letterValueVector) {
+    const a = Number.parseInt(document.querySelector(`#ex03a`).value);
+    const b = Number.parseInt(document.querySelector(`#ex03b`).value);
+    const c = Number.parseInt(document.querySelector(`#ex03c`).value);
+    const d = Number.parseInt(document.querySelector(`#ex03d`).value);
+    const e = Number.parseInt(document.querySelector(`#ex03e`).value);
+    const f = Number.parseInt(document.querySelector(`#ex03f`).value);
+    const l = Number.parseInt(document.querySelector(`#ex03l`).value);
+
+    const matrix = [[a, b], [c, d]];
+    const sumVector = [e, f];
+
+    let resultVector = multiplyMatrixVector(matrix, letterValueVector);
+    resultVector = addVectors(resultVector, sumVector);
+    resultVector = math.mod(resultVector, l);
+    resultVector = convertNumberVectorToLetterVector(resultVector);
+    return resultVector;
+}
+
+function calculate_decoded_vector(letterValueVector) {
+    const a = Number.parseInt(document.querySelector(`#ex03a`).value);
+    const b = Number.parseInt(document.querySelector(`#ex03b`).value);
+    const c = Number.parseInt(document.querySelector(`#ex03c`).value);
+    const d = Number.parseInt(document.querySelector(`#ex03d`).value);
+    const e = Number.parseInt(document.querySelector(`#ex03e`).value);
+    const f = Number.parseInt(document.querySelector(`#ex03f`).value);
+    const l = Number.parseInt(document.querySelector(`#ex03l`).value);
+
+    const matrix = [[a, b], [c, d]];
+
+    function getInverseModulo(number, modulo) {
+        let i = 0;
+        while (i < 100000) {
+            if ((number * i) % modulo === 1) return i;
+            i++;
+        }
+        return undefined;
+    }
+
+    const inverseMatrix = math.inv(matrix);
+    const modInverseMatrix = math.mod(inverseMatrix, l);
+    const subVector = [e, f];
+
+    let resultVector = subtractVectors(letterValueVector, subVector);
+    resultVector = math.mod(resultVector, l);
+    resultVector = multiplyMatrixVector(modInverseMatrix, resultVector);
+    resultVector = math.mod(resultVector, l);
+    resultVector = convertNumberVectorToLetterVector(resultVector);
+    return resultVector;
+}
+
+function encodeVector(vector) {
+    const letterObjectVector = vector.map(value => arrays[EX03ARRAY].find(letter => letter.char === value));
+    const letterValueVector = letterObjectVector.map(value => value.key);
+    return calculate_encoded_vector(letterValueVector);
+}
+
+function decodeVector(vector) {
+    const letterObjectVector = vector.map(value => arrays[EX03ARRAY].find(letter => letter.char === value));
+    const letterValueVector = letterObjectVector.map(value => value.key);
+    return calculate_decoded_vector(letterValueVector);
+}
+
+function negativeModulo(number, modulo) {
+    while (number < 0) {
+        number += modulo;
+    }
+}
+
+const ex03 = (encode, exercise = 3) => {
+    const text = document.querySelector(`#ex0${exercise}text`).value.toLowerCase();
+    const resultDiv = document.querySelector(`#ex0${exercise}results`);
+
+    const textArray = text.split('');
+    const resultArray = [];
+    if (textArray.every(value => letterIsInArrayEx02(value))) {
+        if (textArray.length % 2 !== 0) {
+            textArray.push('a');
+        }
+        for (let i = 0; i < textArray.length; i += 2) {
+            if (encode) {
+                const vector = [textArray[i], textArray[i + 1]];
+                const encodedVector = encodeVector(vector);
+                resultArray.push(encodedVector.join(''));
+            } else {
+                const vector = [textArray[i], textArray[i + 1]];
+                const decodedVector = decodeVector(vector);
+                resultArray.push(decodedVector.join(''));
+            }
+        }
+    } else {
+        alert('text not in array');
+        console.warn('text not in array');
+    }
+
+    resultDiv.textContent = resultArray.join('').toUpperCase();
+}
+
+function convertNumberVectorToLetterVector(numberVector) {
+    return numberVector.map(value => arrays[EX03ARRAY].find(letter => letter.key === value)?.char);
+}
+
+function moduloVector(vector, modulo) {
+    return vector.map(value => value >= 0 ? value % modulo : negativeModulo(value, modulo));
+}
+
+function addVectors(vector1, vector2) {
+    const result = [];
+    if (vector1.length !== vector2.length) console.error('different vectors lengths');
+    else {
+        for (let i = 0; i < vector1.length; i++) {
+            result.push(vector1[i] + vector2[i]);
+        }
+    }
+    return result;
+}
+
+function subtractVectors(vector1, vector2) {
+    const result = [];
+    if (vector1.length !== vector2.length) console.error('different vectors lengths');
+    else {
+        for (let i = 0; i < vector1.length; i++) {
+            result.push(vector1[i] - vector2[i]);
+        }
+    }
+    return result;
+}
+
+function multiplyMatrixVector(matrix, vector) {
+    const result = [];
+    let sum = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i].length !== vector.length) console.error('different matrix.row | vector length');
+            else {
+                sum += matrix[i][j] * vector[j];
+            }
+        }
+        result.push(sum);
+        sum = 0;
+    }
+    return result;
+}
+
+const clearResults = (exercise) => {
+    document.querySelector(`#ex0${exercise}text`).value = '';
+    document.querySelector(`#ex0${exercise}results`).textContent = '';
+
+    if (exercise === 2) {
+        document.querySelector(`#ex0${exercise}a`).value = '';
+        document.querySelector(`#ex0${exercise}b`).value = '';
+        document.querySelector(`#ex0${exercise}l`).value = '';
+
+    }
+}
+
+const updateTable = (exercise) => {
+    const table = document.querySelector(`#ex0${exercise}table`);
+    let formattedTable = '<table><tr>';
+    formattedTable += arrays[exercise - 1].map(value => {
+        if (value.char !== ' ')
+            return `<td>${value.char}</td>`;
+    }).join('').toUpperCase();
+    formattedTable += '</tr><tr>';
+    formattedTable += arrays[exercise - 1].map(value => {
+        if (value.key !== ' ')
+            return `<td>${value.key}</td>`;
+    }).join('').toUpperCase();
+    formattedTable += '</tr></table>';
+    table.innerHTML = formattedTable;
+
+};
+
+const initialPre = (exercise) => {
+    switch (exercise) {
+        case 1:
+            document.querySelector(`#ex0${exercise}results`).textContent = `LICZBA BITÓW
+PREDYKCJA BIEŻĄCA
+
+YWTMŁŚDŁBRŁAMYM
+ĆEMREFTŚŚE`;
+            break;
+        case 2:
+            document.querySelector(`#ex0${exercise}results`).textContent = `SPRZEDAJ SŁONINĘ
+JUTRO TA GODZINA
+
+LUMBLWNHLA
+NHWĄJFIĄŻĘ`;
+            break;
+        case 3:
+            break;
+    }
+}
+
+
+updateTable(1);
+initialPre(1);
+updateTable(2);
+initialPre(2);
+
+/*
+*/
+
+
+
